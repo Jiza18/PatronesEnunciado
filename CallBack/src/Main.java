bb069e441ff5807fea4d8b8c862aa8a15b4b1f3c
alelopdel <alelopdel@alu.edu.gva.es>
2023-03-16 13:39:54 +0100

    .

--- CallBack/src/Main.java
+++ CallBack/src/Main.java
@@ -1,17 +1,10 @@
 class Calculadora {
 
     interface CallBack {
-        void resultado(String resultado);
+        void resultado(int resultado);
         void equivocado(String equivocado);
     }
 
-    void darExplicacion(CallBack callBack) {
-        if (Calculadora.divide(13,0) == 0) {
-            callBack.equivocado("Ha habido un error.");
-        } else {
-            callBack.resultado("El resultado es: ");
-        }
-    }
 
     static int suma(int... numeros) {
         int resultado = 0;
@@ -20,41 +13,41 @@ class Calculadora {
     }
 
 
-    static int divide(int a, int b) {
-        return a/b;     // ¿ y si b es 0 ?
+    static void divide(int a, int b, CallBack callBack) {
+        if (b==0){
+            callBack.equivocado("Dividir por 0");
+        } else {
+            callBack.resultado(a/b);     // ¿ y si b es 0 ?
+        }
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
-
-        int resultadoSuma = Calculadora.suma(1, 2, 3, 10, 20);
-        System.out.println("La suma es: " + resultadoSuma);
-
-
-        int resultadoDivision = Calculadora.divide(13, 2);
-        System.out.println("La division es: " + resultadoDivision);
+//        int resultadoSuma = Calculadora.suma(1, 2, 3, 10, 20);
+//        System.out.println("La suma es: " + resultadoSuma);
+//
+//        int resultadoOtraSuma = Calculadora.suma(1, 2147483646, 1);
+//        System.out.println("La suma es: " + resultadoOtraSuma);
 
 
-        int resultadoOtraSuma = Calculadora.suma(1, 2147483646, 1);
-        System.out.println("La suma es: " + resultadoOtraSuma);
 
-
-        int resultadoOtraDivision = Calculadora.divide(13, 0);
-        System.out.println("La division es: " + resultadoOtraDivision);
-
-        calculadora.darExplicacion(new Calculadora.CallBack() {
+        Calculadora.divide(13, 0, new Calculadora.CallBack() {
             @Override
-            public void resultado(String resultado) {
-                System.out.println(resultado + resultadoSuma);
+            public void resultado(int resultado) {
+                System.out.println("La division es: " + resultado);
             }
+
             @Override
             public void equivocado(String equivocado) {
-                System.err.println(equivocado + resultadoOtraDivision);
+                System.out.println("Error: " + equivocado);
             }
         });
+
+
+
+
 
     }
 }
