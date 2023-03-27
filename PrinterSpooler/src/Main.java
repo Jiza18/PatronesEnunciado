1382206e081159ab0c737975488e32c704f57eda
alelopdel <alelopdel@alu.edu.gva.es>
2023-03-27 09:01:30 +0200

    Patrones || Alex Lopez

---
+++ PrinterSpooler/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class PrinterSpooler {
+    int jobCount;
+    public static PrinterSpooler INSTANCIA;
+    PrinterSpooler() {
+        jobCount = 0;
+    }
+
+    static synchronized PrinterSpooler getInstance(){
+        if(INSTANCIA==null){
+            INSTANCIA=new PrinterSpooler();
+        }
+        return INSTANCIA;
+    }
+    synchronized void printJob(String job) {
+        System.out.println("Printing job " + (++jobCount) + ": " + job);
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws InterruptedException {
+        PrinterSpooler printerSpooler = PrinterSpooler.getInstance();
+
+        List<Callable<Void>> printOrders = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            final int docid = i;
+            printOrders.add(() -> {
+                printerSpooler.printJob("Document" + docid);
+                return null;
+            });
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.invokeAll(printOrders);
+        executorService.shutdown();
+
+        System.out.println("FINISHED. Jobs printed: " + printerSpooler.jobCount);
+    }
+}
